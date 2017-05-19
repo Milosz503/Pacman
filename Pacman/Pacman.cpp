1cb9dad639548d787fa9cfa4270646dd53ac21aa
Milosz <milosz.0518@gmail.com>
2017-05-19 21:13:26 +0200

    Dodanie ImGui

--- Pacman/Pacman.cpp
+++ Pacman/Pacman.cpp
@@ -1,103 +1,185 @@
-#include "ConsoleWindow.h"
-#include "GameObject.h"
-#include "StateStack.h"
+#include "imgui.h"
+#include "imgui-SFML.h"
 
-#include "GameState.h"
-#include "MenuState.h"
-#include "LevelManager.h"
-#include "EditState.h"
-#include "LevelChoiceState.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include "TextureManager.h"
 
-
-//Po setPosition entity moze przeskoczyc przez sciane
-//Brak wsparcia dla przesuniecia swiata prze EntityController
+#include <iostream>
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(1000, 800), "ImGui + SFML = <3");
+	//window.setFramerateLimit(60);
+	ImGui::SFML::Init(window);
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
 	TextureManager textureManager;
 	textureManager.generateTileset();
 	textureManager.loadTextures();
-	ConsoleWindow console(50, 50, &textureManager);
 
+	bool show_test_window = true;
 
-	//Level level;
-
-	LevelManager levelManager;
-
-
-	
-
-	State::Context context(&console, &textureManager);
-
-
-	StateStack stack(context);
-
-	stack.registerState<GameState>(States::Game);
-	stack.registerState<MenuState>(States::Menu);
-	stack.registerState<EditState>(States::Editor);
-	stack.registerState<LevelChoiceState>(States::LevelChoiceGame, States::Game);
-	stack.registerState<LevelChoiceState>(States::LevelChoiceEditor, States::Editor);
-
-	stack.pushState(States::Menu);
-
-
-
-	const sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
-
-	sf::Clock clock;
-
-	sf::Time timeSinceLasUpdate = sf::Time::Zero;
-
-
-	while (console.isOpen())
-	{
+	sf::Clock deltaClock;
+	while (window.isOpen()) {
 		sf::Event event;
-		while (console.pollEvent(event))
-		{
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
 
-			stack.handleEvent(event);
-
-			if (event.type == Event::Closed)
-			{
-				console.close();
+			if (event.type == sf::Event::Closed) {
+				window.close();
 			}
 		}
 
-		sf::Time dt = clock.restart();
-		timeSinceLasUpdate += dt;
+		ImGui::SFML::Update(window, deltaClock.restart());
 
-		while (timeSinceLasUpdate >= TimePerFrame)
+		ImGui::Begin("Hello, world!");
+		ImGui::Button("Look at this pretty button");
+		ImGui::ColorButton(ImVec4(1, 1, 0, 1));
+
+		static float f = 0.0f;
+		ImGui::Text("Hello, world!");
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		
+		sf::Sprite sprite;
+		sprite.setTexture(textureManager.getTileset());
+
+		for (int i = 0; i < 16; ++i)
 		{
-			stack.update(timeSinceLasUpdate);
-			timeSinceLasUpdate -= TimePerFrame;
+			sprite.setTextureRect(sf::IntRect(i*9, 1, 8, 8));
 
-			//stack.update(TimePerFrame);
-
-			if (stack.isEmpty())
+			ImGui::PushID(i);
+			if (ImGui::ImageButton(sprite, sf::Vector2f(16, 16)))
 			{
-				console.close();
+				std::cout << i << std::endl;
 			}
+			ImGui::PopID();
 
-			console.clear(sf::Color::Black);
-			stack.draw();
-			console.show();
+			ImGui::SameLine();
 		}
+		
 
-	
+		ImGui::End();
 
-		/*console.clear(sf::Color::Black);
-		stack.draw();
-		console.show();*/
+		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+		ImGui::ShowTestWindow(&show_test_window);
 
+		window.clear();
+		window.draw(shape);
+		ImGui::Render();
+		window.display();
 	}
 
-
-
-
-
-	
-
-
-	return 0;
+	ImGui::SFML::Shutdown();
 }
+
+
+
+//#include "ConsoleWindow.h"
+//#include "GameObject.h"
+//#include "StateStack.h"
+//
+//#include "GameState.h"
+//#include "MenuState.h"
+//#include "LevelManager.h"
+//#include "EditState.h"
+//#include "LevelChoiceState.h"
+//
+//
+////Po setPosition entity moze przeskoczyc przez sciane
+////Brak wsparcia dla przesuniecia swiata prze EntityController
+//
+//int main()
+//{
+//
+//	TextureManager textureManager;
+//	textureManager.generateTileset();
+//	textureManager.loadTextures();
+//	ConsoleWindow console(50, 50, &textureManager);
+//
+//
+//	//Level level;
+//
+//	LevelManager levelManager;
+//
+//
+//	
+//
+//	State::Context context(&console, &textureManager);
+//
+//
+//	StateStack stack(context);
+//
+//	stack.registerState<GameState>(States::Game);
+//	stack.registerState<MenuState>(States::Menu);
+//	stack.registerState<EditState>(States::Editor);
+//	stack.registerState<LevelChoiceState>(States::LevelChoiceGame, States::Game);
+//	stack.registerState<LevelChoiceState>(States::LevelChoiceEditor, States::Editor);
+//
+//	stack.pushState(States::Menu);
+//
+//
+//
+//	const sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
+//
+//	sf::Clock clock;
+//
+//	sf::Time timeSinceLasUpdate = sf::Time::Zero;
+//
+//
+//	while (console.isOpen())
+//	{
+//		sf::Event event;
+//		while (console.pollEvent(event))
+//		{
+//
+//			stack.handleEvent(event);
+//
+//			if (event.type == Event::Closed)
+//			{
+//				console.close();
+//			}
+//		}
+//
+//		sf::Time dt = clock.restart();
+//		timeSinceLasUpdate += dt;
+//
+//		while (timeSinceLasUpdate >= TimePerFrame)
+//		{
+//			stack.update(timeSinceLasUpdate);
+//			timeSinceLasUpdate -= TimePerFrame;
+//
+//			//stack.update(TimePerFrame);
+//
+//			if (stack.isEmpty())
+//			{
+//				console.close();
+//			}
+//
+//			console.clear(sf::Color::Black);
+//			stack.draw();
+//			console.show();
+//		}
+//
+//	
+//
+//		/*console.clear(sf::Color::Black);
+//		stack.draw();
+//		console.show();*/
+//
+//	}
+//
+//
+//
+//
+//
+//	
+//
+//
+//	return 0;
+//}
